c5c8fefc87724034e9c9f5b1c3237cf1f6d8a2d4
yeseul0 <129267746+yeseul0@users.noreply.github.com>
2026-02-19 20:20:21 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -1,19 +1,20 @@
 class dot:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        
+        self.x=x
+        self.y=y
+
 def solution(dots):
-    #총 검사 3쌍. -> 기울기 같은지 보면 됨
-    a = dot(dots[0][0], dots[0][1])
-    b = dot(dots[1][0], dots[1][1])
-    c = dot(dots[2][0], dots[2][1])
-    d = dot(dots[3][0], dots[3][1])
     
-    if (a.x-b.x)*(c.y-d.y) == (a.y-b.y)*(c.x-d.x):
+    dot1=dot(dots[0][0],dots[0][1])
+    dot2=dot(dots[1][0],dots[1][1])
+    dot3=dot(dots[2][0],dots[2][1])
+    dot4=dot(dots[3][0],dots[3][1])
+    
+    
+    if (dot1.x-dot2.x)*(dot3.y-dot4.y)==(dot1.y-dot2.y)*(dot3.x-dot4.x):
         return 1
-    elif (a.x-c.x)*(b.y-d.y) == (a.y-c.y)*(b.x-d.x):
+    elif (dot1.x-dot3.x)*(dot2.y-dot4.y)==(dot1.y-dot3.y)*(dot2.x-dot4.x):
         return 1
-    elif (a.x-d.x)*(b.y-c.y) == (a.y-d.y)*(b.x-c.x):
+    elif (dot1.x-dot4.x)*(dot2.y-dot3.y)==(dot1.y-dot4.y)*(dot2.x-dot3.x):
         return 1
     else: return 0
