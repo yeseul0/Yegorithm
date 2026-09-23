d689809e97171ea9d12bf80c3ecdca8fec630773
yeseul0 <choiyeseul@g.hongik.ac.kr>
2026-09-23 16:47:37 +0900

    [level 0] Title: 홀짝 구분하기, Time: 3.79 ms, Memory: 4.79 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    if (n%2 == 0) {
+        cout << n <<" is even" << "\n";
+    }
+    else cout << n << " is odd" << "\n";
+    return 0;
+}
