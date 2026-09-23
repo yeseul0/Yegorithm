9b4f25a180093936f24f911024b8db594dbc93d1
yeseul0 <choiyeseul@g.hongik.ac.kr>
2026-09-23 20:00:46 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 4.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_suffix) {
+    if (my_string.size()<is_suffix.size()) return 0;
+    return my_string.substr(my_string.size()-is_suffix.size()) == is_suffix ;
+    
+}
